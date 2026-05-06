63bdc6b7e6b2aaeb0342c2b8537b8b559cf1eac0
arieeliiu <ari.molinam@duocuc.cl>
2026-05-06 14:12:12 -0400

    backend preparado para vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,25 +1,7 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
+import { createLocalApp } from './bootstrap/nest-app';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: 'http://localhost:3001',
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
+  const app = await createLocalApp();
 
   await app.listen(process.env.PORT ?? 3000);
 }
